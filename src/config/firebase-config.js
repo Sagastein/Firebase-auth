d86f4bf7567c27607c7962491a592009a86b7651
Sagacode <ishimwe.sage.3@gmail.com>
2024-03-07 12:58:02 +0200

    clear firebase config secrets

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: "AIzaSyBRRfMBpE1tfX47NxoU7TCXd59ExzgQnso",
-  authDomain: "saga-29037.firebaseapp.com",
-  projectId: "saga-29037",
-  storageBucket: "saga-29037.appspot.com",
-  messagingSenderId: "472072647857",
-  appId: "1:472072647857:web:162f610b146aca5ee09e90",
-  measurementId: "G-6H07BBPL0S",
+  apiKey: "your key",
+  authDomain: "domain",
+  projectId: "id",
+  storageBucket: "storage",
+  messagingSenderId: "sender id",
+  appId: "app id",
+  measurementId: "measurement id",
 };
 
 const app = initializeApp(firebaseConfig);
